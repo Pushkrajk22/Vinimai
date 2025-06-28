ff886f3e9038ec45c2f597a411dde2d010f4dee7
Pushkraj Kulkarni <pushkraj.kulkarni@ibm.com>
2025-06-29 01:00:31 +0530

    added contact details

--- src/app/pages/contact/page.tsx
+++ src/app/pages/contact/page.tsx
@@ -39,15 +39,17 @@ const ContactUs = () => {
                         <div className="right lg:w-1/4 lg:pl-4">
                             <div className="item">
                                 <div className="heading4">Our Store</div>
-                                <p className="mt-3">2163 Phillips Gap Rd, West Jefferson, North Carolina, United States</p>
-                                <p className="mt-3">Phone: <span className='whitespace-nowrap'>+1 666 8888</span></p>
-                                <p className="mt-1">Email: <span className='whitespace-nowrap'>hi.avitex@gmail.com</span></p>
+                                <p className="mt-3">Bhyander West, Mumbai</p>
+                                <p className="mt-3">Phone: <span className='whitespace-nowrap'>+91 93211 82959</span></p>
+                                <p className="mt-1">Email: <span className='whitespace-nowrap'>parthivgandhi4@gmail.com</span></p>
                             </div>
                             <div className="item mt-10">
                                 <div className="heading4">Open Hours</div>
-                                <p className="mt-3">Mon - Fri: <span className='whitespace-nowrap'>7:30am - 8:00pm PST</span></p>
+                                <p className="mt-3">We are always available for you.<span className='whitespace-nowrap'></span></p>
+                                <p className="mt-3">Just a click away...<span className='whitespace-nowrap'></span></p>
+                                {/* <p className="mt-3">Mon - Fri: <span className='whitespace-nowrap'>7:30am - 8:00pm PST</span></p>
                                 <p className="mt-3">Saturday: <span className='whitespace-nowrap'>8:00am - 6:00pm PST</span></p>
-                                <p className="mt-3">Sunday: <span className='whitespace-nowrap'>9:00am - 5:00pm PST</span></p>
+                                <p className="mt-3">Sunday: <span className='whitespace-nowrap'>9:00am - 5:00pm PST</span></p> */}
                             </div>
                         </div>
                     </div>
